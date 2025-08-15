2ba512f638dfde87e57efaa00d38cc9bda43e80f
Ershov Maxim <ershov_mm@taximaxim.ru>
2025-08-15 16:05:56 +0500

    broken docker

--- server/app/main.py
+++ server/app/main.py
@@ -1,21 +1,32 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from contextlib import asynccontextmanager
 
+from app.models import Base
+from app.db import engine
 from .routes import api
 
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+    yield
 
-app = FastAPI()
+    # Shutdown: (optional) close resources here
+    await engine.dispose()
+        
+app = FastAPI(lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-    "http://localhost:3000",
-    "http://127.0.0.1:3000",
-    "http://0.0.0.0:3000"],       # или ["*"] на время отладки
+        "http://localhost:3000",
+        "http://127.0.0.1:3000",
+        "http://0.0.0.0:3000"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
 
 app.include_router(api.router)
